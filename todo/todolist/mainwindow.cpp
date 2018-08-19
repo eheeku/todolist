957b8fc896b1f913a46ad5576763e8bba902aa0f
eheeku <eheeku@gmail.com>
2018-08-19 13:37:33 +0900

    push btn test

--- todo/todolist/mainwindow.cpp
+++ todo/todolist/mainwindow.cpp
@@ -5,10 +5,15 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->addTodo->setText("hello eheeku!");
+    //ui->addTodo->setText("hello eheeku!");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_saveBtn_clicked()
+{
+    ui->label_2->setText("hi!");
 }
